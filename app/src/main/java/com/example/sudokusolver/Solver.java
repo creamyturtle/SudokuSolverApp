dc1f18f24211be509bcf2cd520444f856783edf9
creamyturtle <peterdaveloose@gmail.com>
2021-03-28 17:36:00 -0400

    Updated Sudoku Solver App w/ suggested code fix.  not working

--- app/src/main/java/com/example/sudokusolver/Solver.java
+++ app/src/main/java/com/example/sudokusolver/Solver.java
@@ -119,7 +119,7 @@ public class Solver {
 
 
     public void setNumberPos(int num) {
-        if (this.selected_row != -1 && this.selected_column != -1){
+        if (this.selected_row != -1 && this.selected_column != -1 && !check(this.selected_row - 1, this.selected_column - 1)){
             if (this.board[this.selected_row-1][this.selected_column-1] == num) {
                 this.board[this.selected_row-1][this.selected_column-1] = 0;
 
@@ -127,6 +127,7 @@ public class Solver {
             else{
                 this.board[this.selected_row-1][this.selected_column-1] = num;
             }
+
         }
     }
 
